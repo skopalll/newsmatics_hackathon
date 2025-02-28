f707cb61e07f115edc57d30aa67bb5dbddb8e67f
Maxmilian Petrov <maxa@mail.muni.cz>
2025-02-28 20:37:36 +0100

    more db helpers

--- backend/helpers.py
+++ backend/helpers.py
@@ -23,7 +23,7 @@ def connect_db():
     conn = sqlite3.connect(DB_FILE)
     return conn
 
-def create_table():
+def create_topics_table():
     """Creates a sample users table if not exists"""
     conn = connect_db()
     cursor = conn.cursor()
@@ -66,12 +66,68 @@ def update_topic(id, date, title, text):
     conn.close()
     log(f"Updated topic {id}: {title} on {date}")
 
-def get_topics():
-    """Fetches all users from the database"""
+def get_topic(id):
+    """Fetches a topic by its id from the database"""
     conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM topics")
-    users = cursor.fetchall()
+    cursor.execute("SELECT * FROM topics WHERE id = ?", (id,))
+    topic = cursor.fetchall()
     conn.close()
-    return users
+    return topic
+
+def create_keywords_table():
+    """Creates a sample users table if not exists"""
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS keywords (
+                        id INTEGER PRIMARY KEY,
+                        keywords TEXT NOT NULL,
+                        FOREIGN KEY (id) REFERENCES topics(id))''')
+    conn.commit()
+    conn.close()
+    log("Database table created successfully")
+
+def add_keywords(id, keywords):
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO keywords (id, keywords) VALUES (?, ?)", (id, keywords))
+        conn.commit()
+        conn.close()
+        log(f"Added keywords for topic {id}: {keywords}")
+    except sqlite3.IntegrityError:
+        log(f"Failed to add keywords for topic {id}: Integrity error", "warning")
+
+def update_keywords(id, keywords):
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("UPDATE keywords SET keywords = ?, WHERE id = ?", (keywords, id))
+        conn.commit()
+        conn.close()
+        log(f"Added keywords for topic {id}: {keywords}")
+    except sqlite3.IntegrityError:
+        log(f"Failed to add keywords for topic {id}: Integrity error", "warning")
+
+def delete_keywords(id):
+    """Deletes keywords for a given topic by its id"""
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM keywords WHERE id = ?", (id,))
+        conn.commit()
+        conn.close()
+        log(f"Deleted keywords for topic {id}")
+    except sqlite3.Error as e:
+        log(f"Failed to delete keywords for topic {id}: {e}", "warning")
+
+def get_keywords(id):
+    """Fetches a keyword by its id from the database"""
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM topics WHERE id = ?", (id,))
+    topic = cursor.fetchall()
+    conn.close()
+    return topic
+
 
