a781e5856488d5f14fedc2c3f9ff44a1c88fc489
Maxmilian Petrov <maxa@mail.muni.cz>
2025-03-01 00:16:53 +0100

    localize

--- backend/localize.py
+++ backend/localize.py
@@ -1,17 +1,10 @@
 from geopy.geocoders import Nominatim
 
-def get_publisher_latlong(address):
+def get_publisher_latlong(cityandstate):
     geolocator = Nominatim(user_agent="newsm_hackathon_!/1.0")  # Provide a unique user agent
-    location = geolocator.geocode(address)
+    location = geolocator.geocode(cityandstate)
     if location:
         return location.latitude, location.longitude
     else:
         return None  # Return None if the address could not be found
 
-# Example usage:
-
-lat, long = get_publisher_latlong("New York, New York")
-if lat and long:
-    print(f"Latitude: {lat}, Longitude: {long}")
-else:
-    print("Address not found.")
